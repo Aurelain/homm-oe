92238fbacca9c45c72dbeedce60ce14fa87f7898
aurelain <aurelain@gmail.com>
2026-05-17 14:36:35 +0300

    flattened the paths

--- scripts/editWiki/helpers/inferPageTitle.js
+++ scripts/editWiki/helpers/inferPageTitle.js
@@ -7,7 +7,8 @@ function inferPageTitle() {
     let pageTitle = TARGET;
     pageTitle = pageTitle.replace(/.*?wiki\//, ''); // remove prefix path
     pageTitle = pageTitle.replace(/\.\w+$/, ''); // remove extension
-    pageTitle = pageTitle.replace(/~/, ':'); // replace the tilde with colon
+    pageTitle = pageTitle.replace(/#/, ':'); // replace the hash with colon
+    pageTitle = pageTitle.replace(/~/, '/'); // replace the tilde with slash
     return pageTitle;
 }
 
