a35f0825b755370bec68c8515b63f2b2fa9dae32
aurelain <aurelain@gmail.com>
2026-05-22 19:51:39 +0300

    improved the Laws module

--- scripts/editWiki/wiki/Module/Sandbox~Aurelain~Laws.lua
+++ scripts/editWiki/wiki/Module/Sandbox~Aurelain~Laws.lua
@@ -8,6 +8,14 @@ local FACTION_ORDER = {
     unfrozen = 5,
     dungeon = 6,
 }
+local SIDE_RANK = {
+    faction = 1,
+    army = 2,
+}
+local SIDE_ICONS = {
+    faction = 'Sub_skill_economy_1_icon',
+    army = 'Sub_skill_battlemage_4_icon',
+}
 
 ------------------------------------------------------------------------------------------------------------------------
 -- Displays a variable
@@ -50,7 +58,7 @@ local function queryMain(lang)
     local cargoArgs = {
         join = 'Law.id = Translation.target_id',
         where = 'Translation.language = "' .. lang .. '"',
-        limit = 10
+        limit = 1000
     }
     return mw.ext.cargo.query(tables, fields, cargoArgs)
 end
@@ -67,12 +75,12 @@ local function consolidateMain(results)
         local entry = hub[id]
         if not entry then
             entry = {}
-            entry.faction = row.faction
+            entry.faction = row.faction or ''
             entry.tier = row.tier
             entry.icon = row.icon
             entry.costs = nil -- added later on by `addCosts()`
-            entry.side = nil -- added later on by `addPositions()`
-            entry.slot = nil -- added later on by `addPositions()`
+            entry.side = ''  -- added later on by `addPositions()`
+            entry.slot = 0    -- added later on by `addPositions()`
             hub[id] = entry
         end
         if row.name then
@@ -110,7 +118,7 @@ local function addPositions(hub)
         local entry = hub[id]
         if entry then
             entry.side = row.side
-            entry.slot = row.slot
+            entry.slot = row.slot or 0
         end
     end
 end
@@ -129,12 +137,89 @@ end
 ------------------------------------------------------------------------------------------------------------------------
 --
 ------------------------------------------------------------------------------------------------------------------------
-local function mergeDescriptions(desc1, desc2, desc3)
-    --for i = 1, #desc1 do
-    --    local char = string.sub(desc1, i, i) -- Slices from index i to i
-    --    print(char)
-    --end
-    return desc1
+local function checkEquality(chars)
+    local recent = nil
+    for _, c in pairs(chars) do
+        if recent == nil then
+            recent = c
+        elseif recent ~= c then
+            return false
+        end
+    end
+    return true
+end
+
+------------------------------------------------------------------------------------------------------------------------
+--
+------------------------------------------------------------------------------------------------------------------------
+local function findNextNonDigit(text, index)
+    for i = index + 1, #text do
+        local char = string.sub(text, i, i)
+        if string.match(char, "%D") then
+            return i
+        end
+    end
+    return nil
+end
+
+------------------------------------------------------------------------------------------------------------------------
+--
+------------------------------------------------------------------------------------------------------------------------
+local function advanceActors(actors)
+    local output = {};
+    for _, actor in pairs(actors) do
+        local cursor = actor.cursor
+        local next = findNextNonDigit(actor.text, cursor)
+        local skipped = string.sub(actor.text, cursor, next)
+        actor.cursor = next
+        table.insert(output, '<span>' .. skipped .. '</span>')
+    end
+    return '<span class="island">' .. table.concat(output, '/') .. '</span>'
+end
+
+------------------------------------------------------------------------------------------------------------------------
+--
+------------------------------------------------------------------------------------------------------------------------
+local function mergeDescriptions(candidates)
+    local actors = {};
+    for _, text in ipairs(candidates) do
+        if text then
+            table.insert(actors, {
+                text = text,
+                cursor = 0
+            })
+        end
+    end
+    local output = {}
+    for _ = 1, 1000 do
+        local currentChar = {}
+        for i, actor in ipairs(actors) do
+            actor.cursor = actor.cursor + 1
+            currentChar[i] = string.sub(actor.text, actor.cursor, actor.cursor)
+        end
+        local c = currentChar[1]
+        if c == '' then
+            break
+        end
+        if not checkEquality(currentChar) then
+            local island = advanceActors(actors)
+            table.insert(output, island)
+        else
+            table.insert(output, currentChar[1])
+        end
+    end
+    return table.concat(output, '')
+end
+
+------------------------------------------------------------------------------------------------------------------------
+--
+------------------------------------------------------------------------------------------------------------------------
+local function mergeCosts(costs)
+    local output = {}
+    for _, cost in ipairs(costs) do
+        table.insert(output, '<span>' .. cost .. '</span>')
+    end
+    return '<span class="island">' .. table.concat(output, '/') .. '</span>'
 end
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -142,9 +227,8 @@ end
 ------------------------------------------------------------------------------------------------------------------------
 local function mergeTexts(laws)
     for _, row in ipairs(laws) do
-        local costs = row.costs
-        row.costs = table.concat(costs, "/")
-        row.desc1 = mergeDescriptions(row.desc1, row.desc2, row.desc3);
+        row.costsText = mergeCosts(row.costs)
+        row.description = mergeDescriptions({ row.desc1, row.desc2, row.desc3 });
     end
 end
 
@@ -159,14 +243,15 @@ local function sortLaws(a, b)
         return rankA < rankB
     end
 
-    if a.tier ~= a.tier then
-        return a.tier < a.tier
+    if a.tier ~= b.tier then
+        return a.tier < b.tier
     end
 
-    --local sideRankA = a.side == ''
-    --if a.upgradeRank ~= unitB.upgradeRank then
-    --    return unitA.upgradeRank < unitB.upgradeRank
-    --end
+    local sideRankA = SIDE_RANK[a.side] or 99
+    local sideRankB = SIDE_RANK[b.side] or 99
+    if sideRankA ~= sideRankB then
+        return sideRankA < sideRankB
+    end
 
     return a.slot < b.slot
 end
@@ -201,15 +286,32 @@ end
 ------------------------------------------------------------------------------------------------------------------------
 --
 ------------------------------------------------------------------------------------------------------------------------
-local function createBody(htmlTable, laws)
+local function createBody(htmlTable, laws, frame)
+    local currentTier = laws[1].tier
+    local currentFaction = laws[1].faction
     for _, u in ipairs(laws) do
+        local factionName = frame:preprocess('{{F|' .. u.faction .. '}}')
+
+        -- separators
+        if u.faction ~= currentFaction then
+            currentFaction = u.faction
+            currentTier = u.tier
+            local separator = htmlTable:tag('tr'):addClass('separator'):addClass('separator-large')
+            separator:tag('td'):attr('colspan', 6):wikitext(factionName):done()
+        elseif u.tier ~= currentTier then
+            currentTier = u.tier
+            local separator = htmlTable:tag('tr'):addClass('separator'):addClass('separator-tiny')
+            separator:tag('td'):attr('colspan', 6):done()
+        end
+
+        local sideIcon = SIDE_ICONS[u.side]
         local tr = htmlTable:tag('tr')
         addTd(tr, u.name)
-        addTd(tr, u.faction)
+        addTd(tr, factionName)
         addTd(tr, u.tier)
-        addTd(tr, u.side)
-        addTd(tr, u.desc1)
-        addTd(tr, u.costs)
+        addTd(tr, sideIcon and '[[File:' .. sideIcon .. '.png|24px|' .. u.side .. '|link=]]')
+        addTd(tr, u.description)
+        addTd(tr, u.costsText .. '[[File:Icon_LawsPoint.png|link=]]')
     end
 end
 
@@ -231,17 +333,15 @@ function p.display(frame)
     mergeTexts(laws)
     table.sort(laws, sortLaws)
 
-    --local laws = consolidateResults(results)
-
     -- Table
     local htmlTable = mw.html.create('table')
     htmlTable:addClass('wikitable sortable')
     createHeader(htmlTable)
-    createBody(htmlTable, laws)
+    createBody(htmlTable, laws, frame)
 
-    --return dump(laws)
     -- Output
     local styleTag = frame:extensionTag('templatestyles', '', { src = frame:getTitle() .. '/styles.css' })
+    --return dump(htmlTable)
     return styleTag .. tostring(htmlTable)
 end
 
